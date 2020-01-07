26ed31f8d92903f558485f196ca11f65f45ed8bf
lakshayspeakstocomputer <lakshaysagarrana@gmail.com>
2020-01-07 10:37:48 +0530

    made a simple calulator (very simple)

---
+++ js/calculator.js
@@ -0,0 +1,12 @@
+var a = Number(prompt("Enter firsdt number"));
+var b = Number(prompt("Enter Second Number"));
+
+var sumOfNum = a + b;
+var sub = a - b;
+var mult = a * b;
+var divi = a / b
+
+console.log(sumOfNum)
+console.log(sub)
+console.log(mult)
+console.log(divi)
