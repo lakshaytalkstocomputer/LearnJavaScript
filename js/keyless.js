be47c5cea546cc9eca97c6a309bf9d8a4328b86b
lakshayspeakstocomputer <lakshaysagarrana@gmail.com>
2020-01-12 19:28:17 +0530

    done condition using keyless

---
+++ js/keyless.js
@@ -0,0 +1,13 @@
+var age = Number(prompt("Enter age!"))
+
+if (age < 18)
+{
+    alert("Sorry, you are too young to drive this car. Powering off")
+
+}
+else if(age > 18){
+    alert ("Powering On. Enjoy the ride!")
+}
+else{
+    alert("Congratulations on your first year of driving. Enjoy the ride!")
+}
